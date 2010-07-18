98b18489386f8c514c650168793f6c64c0bd7f34
Alexander Vasilev <sashok@nix.pp.ru>
2010-07-19 00:10:43 +0400

    Converted generator to new Rails3 format (via Thor)

--- lib/generators/loops/loops_generator.rb
+++ lib/generators/loops/loops_generator.rb
@@ -1,24 +1,27 @@
-# This generator bootstraps a Rails project for use with loops
-class LoopsGenerator < Rails::Generators::Base
-  def manifest
-    record do |m|
-      # Generate app/loops directory and an example loop files
-      m.directory 'app'
-      m.directory 'app/loops'
-      m.file      'app/loops/APP_README',     'app/loops/README'
-      m.file      'app/loops/simple_loop.rb', 'app/loops/simple_loop.rb'
-      m.file      'app/loops/queue_loop.rb',  'app/loops/queue_loop.rb'
-      
-      # Generate script/loops file
-      m.directory 'script'
-      m.file      'script/loops',             'script/loops', :chmod => 0755
-      
-      # Generate config/loops.yml file
-      m.directory 'config'
-      m.file      'config/loops.yml',         'config/loops.yml'
-    end
-  end
+require 'rails/generators'
 
+class LoopsGenerator < Rails::Generators::Base
+  
+  def self.source_root
+    File.join(File.dirname(__FILE__), 'templates')
+  end
+  
+  def install_basic_samples
+    # m.directory 'app'
+    # m.directory 'app/loops'
+    copy_file      'app/loops/APP_README',     'app/loops/README'
+    copy_file      'app/loops/simple_loop.rb', 'app/loops/simple_loop.rb'
+    copy_file      'app/loops/queue_loop.rb',  'app/loops/queue_loop.rb'
+  end
+  
+  def install_script
+    copy_file      'script/loops',             'script/loops'
+  end
+  
+  def install_config
+    copy_file      'config/loops.yml',         'config/loops.yml'
+  end
+  
 protected
 
   def banner
